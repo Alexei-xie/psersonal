22cd2da887ea0665184ac63fbeba7454533d2297
duJi <x15237168213@163.com>
2023-09-23 16:32:47 +0800

    add : 添加test服务

---
+++ test/src/main/java/com/itself/Main.java
@@ -0,0 +1,8 @@
+package com.itself;/**
+ * @Author duJi
+ * @Date ${YEAR}/${MONTH}/${DAY}
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
