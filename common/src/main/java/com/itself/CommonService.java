4a6f4333e97c60d87f86c5af60bd68d4016e00fe
duJi <x15237168213@163.com>
2023-10-04 20:17:51 +0800

    fix:依赖缺失

--- common/src/main/java/com/itself/CommonService.java
+++ common/src/main/java/com/itself/CommonService.java
@@ -3,13 +3,12 @@ package com.itself;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * @Author duJi
  * @Date 2023/09/23
  */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})//取消数据库配置
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})//取消数据库配置
 public class CommonService {
     public static void main(String[] args) {
         SpringApplication.run(CommonService.class,args);
