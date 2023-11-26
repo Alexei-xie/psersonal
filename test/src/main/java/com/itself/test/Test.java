e1803c5f177321c7ec162460b3af9e023bc52fe8
duJi <x15237168213@163.com>
2023-11-26 21:14:10 +0800

    opt：list集合方法

--- test/src/main/java/com/itself/test/Test.java
+++ test/src/main/java/com/itself/test/Test.java
@@ -1,8 +1,6 @@
 package com.itself.test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author duJi
@@ -11,7 +9,8 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 4, 5, 6);
-        Collections.swap(list,2,4);
+        System.out.println(list.subList(1,2));//list集合切分收集
+        Collections.swap(list,2,4);//list元素位置调换
         System.out.println(list);
     }
 }
