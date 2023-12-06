95c15ae67f44b90509b055ce6db5a71c201b286f
duJi <x15237168213@163.com>
2023-12-06 14:56:16 +0800

    add：interceptor接口补充

---
+++ example/src/main/java/com/itself/interceptor/TestInterceptorController.java
@@ -0,0 +1,21 @@
+package com.itself.interceptor;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author duJi
+ * @Date 2023/12/06
+ */
+@RestController
+@RequestMapping("interceptor")
+@Api(tags = "拦截器测试接口")
+public class TestInterceptorController {
+
+    @GetMapping("/test")
+    public String testInterceptor(){
+        return "success";
+    }
+}
