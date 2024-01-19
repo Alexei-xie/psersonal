59d4aa76b32cc278a666ceb0f4cb753dd20da352
duJi <x15237168213@163.com>
2024-01-19 18:01:17 +0800

    add:mapstruct依赖方法补充

---
+++ example/src/main/java/com/itself/mapstruct/bean/BeanDto.java
@@ -0,0 +1,23 @@
+package com.itself.mapstruct.bean;
+
+import java.util.List;
+import java.util.Set;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author: duJi
+ * @Date: 2024-01-19
+ **/
+@Data
+@Accessors(chain = true)
+public class BeanDto {
+
+    private String name;
+
+    private String age;
+
+    private List<String> list;
+
+    private Set<String> set;
+}
---
+++ example/src/main/java/com/itself/mapstruct/convert/BeanConvert.java
@@ -0,0 +1,12 @@
+package com.itself.mapstruct.convert;
+
+import org.mapstruct.Mapper;
+
+/**
+ * @Author: duJi
+ * @Date: 2024-01-19
+ **/
+@Mapper
+public interface BeanConvert{
+
+}
