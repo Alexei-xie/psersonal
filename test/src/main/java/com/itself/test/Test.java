bf2b2190c7bbf48d3077c7789b84e6e81bea0e65
duJi <x15237168213@163.com>
2024-05-06 15:23:07 +0800

    feature: 添加多sheet页导入实现逻辑

--- test/src/main/java/com/itself/test/Test.java
+++ test/src/main/java/com/itself/test/Test.java
@@ -1,17 +1,14 @@
 package com.itself.test;
 
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Author duJi
  * @Date 2023/11/05
  */
-@Slf4j
 public class Test {
     public static void main(String[] args) throws IOException, InterruptedException {
         String str = "111";
-        log.info("数据字典管理导入失败， 错误信息： {}",str);
         System.out.println(str);
     }
 }
