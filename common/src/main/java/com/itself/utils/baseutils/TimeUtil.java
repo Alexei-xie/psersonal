88ab9ec33db83c174bd211b68de6daa75c08cda5
duJi <x15237168213@163.com>
2024-05-10 10:59:55 +0800

    add:补充TimeUtil中的方法

--- common/src/main/java/com/itself/utils/baseutils/TimeUtil.java
+++ common/src/main/java/com/itself/utils/baseutils/TimeUtil.java
@@ -1,9 +1,10 @@
-package com.itself.utils;
+package com.itself.utils.baseutils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
@@ -21,15 +22,18 @@ import java.util.concurrent.TimeUnit;
  * @Author xxw
  * @Date 2022/10/14
  */
-public class TimeUtils {
+public class TimeUtil {
 
     private static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
     private static final String DAY_FORMAT = "yyyy-MM-dd";
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
     private static final SimpleDateFormat sdf2 = new SimpleDateFormat("MMdd");
-    private static final SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat sdf3 = new SimpleDateFormat(DAY_FORMAT);
+    private static final SimpleDateFormat sdf4 = new SimpleDateFormat(DAY_FORMAT);
+    private static final SimpleDateFormat sdf5 = new SimpleDateFormat(DEFAULT_FORMAT);
 
-
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DEFAULT_FORMAT);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER_SIMPLE = DateTimeFormatter.ofPattern(DAY_FORMAT);
     /**
      * Long类型的日期转为指定格式的字符串类型
      */
@@ -39,6 +43,61 @@ public class TimeUtils {
     }
 
 
+    /**
+     * 获取一天的开始时间
+     * @param dateTime：字符串时间
+     * @return 返回样式：2022-02-01 00:00:00
+     */
+    public static String getBeginOfDay(String dateTime){
+        // 解析日期字符串
+        LocalDate localDate = formatDateTime(dateTime);
+        LocalDateTime date = LocalDateTime.of(localDate, LocalTime.MIN);
+        return date.format(DATE_TIME_FORMATTER);
+    }
+    /**
+     * 获取一天的末尾时间
+     * @param dateTime：字符串时间
+     * @return 返回样式：2022-02-01 23:59:59
+     */
+    public static String getEndOfDay(String dateTime){
+        // 解析日期字符串
+        LocalDate localDate = formatDateTime(dateTime);
+        LocalDateTime date = LocalDateTime.of(localDate, LocalTime.MAX);
+        return date.format(DATE_TIME_FORMATTER);
+    }
+
+    /**
+     * 日期格式处理转为LocalDate类型：具体到天
+     * @param dateTime 字符串时间：2022-02-22 23:59:59 或 2022-02-22
+     * @return LocalDate类型 样式：2022-02-22
+     */
+    public static LocalDate formatDateTime(String dateTime){
+        //处理字符串前后的空格
+        String date = dateTime.trim();
+        if (date.contains(" ")){
+            return LocalDate.parse(date, DATE_TIME_FORMATTER);
+        }else {
+            return LocalDate.parse(date, DATE_TIME_FORMATTER_SIMPLE);
+        }
+    }
+    /**
+     * 日期格式处理转为Date类型：具体到天
+     * @param dateTime 字符串时间：2022-02-22 23:59:59 或 2022-02-22
+     * @return Date 样式：Tue Feb 01 00:00:00 CST 2022
+     */
+    public static Date formatToDate(String dateTime){
+        //处理字符串前后的空格
+        String date = dateTime.trim();
+        try {
+            if (date.contains(" ")){
+                return sdf5.parse(date);
+            }else {
+                return sdf4.parse(date);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * 字符串类型日期转为 LocalDate类型
@@ -143,15 +202,20 @@ public class TimeUtils {
 
 class TimeUtilDemo{
     public static void main(String[] args) {
+        Date day = TimeUtil.formatToDate("2022-02-01 ");
+        System.out.println(day);
+
         /*LocalDate now = LocalDate.now();
         System.out.println(Date.from(now.atStartOfDay(ZoneId.systemDefault()).toInstant()));
         LocalDate date = getNextDate(now, 1, "month");
         System.out.println(Date.from(date.atStartOfDay(ZoneId.systemDefault()).toInstant()));*/
-        Calendar calendar = Calendar.getInstance();
+
+        /*Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE,1); //此时如果1是负数的时候即(-1)： 日期往前移一天
         System.out.println(calendar.getTime());
         Long s = timeMillisToStr(new Date().getTime());
-        System.out.println(s.equals(20221014L));
+        System.out.println(s.equals(20221014L));*/
+
     }
     public static Long timeMillisToStr(Long dateTime){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
