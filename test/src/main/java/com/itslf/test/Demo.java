8be0a59c1b8ab70bd0a402910364e6e78fd3dac2
duJi <x15237168213@163.com>
2024-05-21 14:31:41 +0800

    enhance：添加误删代码

---
+++ test/src/main/java/com/itslf/test/Demo.java
@@ -0,0 +1,11 @@
+package com.itslf.test;
+
+/**
+ * @Author: duJi
+ * @Date: 2024-05-21
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(2222);
+    }
+}
---
+++ test/src/main/java/com/itslf/test/Test.java
@@ -0,0 +1,11 @@
+package com.itslf.test;
+
+/**
+ * @Author: duJi
+ * @Date: 2024-05-21
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
