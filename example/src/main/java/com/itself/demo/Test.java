9eb6c0156a6da2b0fcfbb89082f375f199cec06f
duJi <x15237168213@163.com>
2024-05-23 14:09:03 +0800

    opt:删除test模块，在example模块下添加demo包供个人写联系代码

--- example/src/main/java/com/itself/demo/Test.java
+++ example/src/main/java/com/itself/demo/Test.java
@@ -1,15 +1,13 @@
-package com.itslf.test;
+package com.itself.demo;
 
 import java.util.List;
 
 /**
  * @Author: duJi
- * @Date: 2024-05-21
+ * @Date: 2024-05-23
  **/
 public class Test {
     public static void main(String[] args) {
-        List<Integer> list = List.of();
-
-        System.out.println(list);
+        System.out.println(List.of(1,23,3));
     }
 }
