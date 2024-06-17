68385b1c61a471f394d3ca3ce6c85cdfb037bfdb
duJi <x15237168213@163.com>
2024-06-17 14:15:32 +0800

    feat:在common模块中添加个人demo的test方法

---
+++ common/src/test/java/com/itself/test/TestDemo.java
@@ -0,0 +1,14 @@
+package com.itself.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: duJi
+ * @Date: 2024-06-17
+ **/
+public class TestDemo {
+    @Test
+    public void test(){
+
+    }
+}
