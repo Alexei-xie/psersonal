d2addef02189125bd10f4fba4ec650b9af93c3a7
Alexei <x15237168213@163.com>
2024-12-28 18:02:57 +0800

    enh：代码优化

--- example/src/main/java/com/itself/easyexcel/FillTemplateController.java
+++ example/src/main/java/com/itself/easyexcel/FillTemplateController.java
@@ -30,6 +30,7 @@ import java.util.Map;
  * ApiFox请求
  * 数据模板填充--模板数据填充示例.xlsx
  * https://blog.csdn.net/btt2013/article/details/121523856
+ *
  * @Author duJi
  * @Date 2024-10-12
  */
@@ -38,55 +39,47 @@ import java.util.Map;
 @RequestMapping("/excelFillData")
 public class FillTemplateController {
 
+    /**
+     * try-with-resources：
+     * 自动关闭 InputStream 和 OutputStream，简化资源管理，避免资源泄漏。
+     * ExcelWriter 也实现了 Closeable 接口，因此也可以使用 try-with-resources 自动关
+     * @param file
+     * @param response
+     */
     @ApiOperation("fillTemplate")
     @PostMapping(value = "/fillTemplate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void fillTemplate(@RequestParam("file") MultipartFile file, HttpServletResponse response)  {
-        OutputStream outputStream = null;
-        InputStream inputStream = null;
-        try {
-        inputStream = file.getInputStream();
-        String fileName = file.getOriginalFilename();// 模板数据填充示例.xlsx
-        String prefixName = FileUtil.mainName(fileName);// 模板数据填充示例
-        String fileType = FileUtil.extName(fileName);
-        // 目标文件名称
-        String targetFileName = prefixName + "-" + DateUtil.format(new Date(), "yyyyMMdd") + "." + fileType;// 模板数据填充示例-2024-10-12-12:12:12.xlsx
-        // Map<String, String> map = new HashMap<>();
-        // map.put("name", "李四" );
-        // map.put("age", "11" );
-        // map.put("remark", "test" );
-        // 设置响应头
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        response.setCharacterEncoding("UTF-8");
-        String encodedFileName = URLEncoder.encode(targetFileName, "UTF-8");
-        response.setHeader("Content-Disposition", "attachment; filename=" + encodedFileName);
+    public void fillTemplate(@RequestParam("file") MultipartFile file, HttpServletResponse response) {
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("上传的文件不能为空");
+        }
+        try (InputStream inputStream = file.getInputStream();
+             OutputStream outputStream = response.getOutputStream()) {
+            String fileName = file.getOriginalFilename();// 模板数据填充示例.xlsx
+            String prefixName = FileUtil.mainName(fileName);// 模板数据填充示例
+            String fileType = FileUtil.extName(fileName);
+            // 目标文件名称
+            String targetFileName = prefixName + "-" + DateUtil.format(new Date(), "yyyyMMdd") + "." + fileType;// 模板数据填充示例-2024-10-12-12:12:12.xlsx
+            // Map<String, String> map = new HashMap<>();
+            // map.put("name", "李四" );
+            // map.put("age", "11" );
+            // map.put("remark", "test" );
+            // 设置响应头
+            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            response.setCharacterEncoding("UTF-8");
+            String encodedFileName = URLEncoder.encode(targetFileName, "UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + encodedFileName);
 
-        // 获取输出流
-        outputStream = response.getOutputStream();
-        // 准备Map数据填充
-        List<Map<String, String>> data = this.initData();
-        // 生成工作簿对象
-        ExcelWriter excelWriter = EasyExcel.write(outputStream).withTemplate(inputStream).build();
-        WriteSheet writeSheet = EasyExcel.writerSheet(0).build();
-        excelWriter.fill(new FillWrapper("stu", data), writeSheet);
-        excelWriter.finish();
-        outputStream.flush();
+            // 准备Map数据填充
+            List<Map<String, String>> data = this.initData();
+            // 生成工作簿对象
+            // 使用 EasyExcel 填充模板并生成文件
+            try (ExcelWriter excelWriter = EasyExcel.write(outputStream).withTemplate(inputStream).build()) {
+                WriteSheet writeSheet = EasyExcel.writerSheet(0).build();
+                excelWriter.fill(new FillWrapper("stu", data), writeSheet);
+                excelWriter.finish();
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (outputStream != null) {
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            throw new RuntimeException("文件处理失败", e);
         }
     }
 
